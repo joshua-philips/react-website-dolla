4f16c4e8cb91f53049eaffce2f026f20f5620e5e
joshuaphilips <philipsjoshua96@gmail.com>
2021-10-20 18:03:00 +0000

    scroll to top & finish

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
 import SignInPage from './pages/signin';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop></ScrollToTop>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SignInPage} exact />
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -16,19 +16,18 @@ const SignIn = () => {
     <>
       <Container>
         <FormWrap>
-          <Icon>
-            <FormContent>
-              <Form action="#">
-                <FormH1>Sign in to your account</FormH1>
-                <FormLabel htmlFor="for">Email</FormLabel>
-                <FormInput type="email" required></FormInput>
-                <FormLabel htmlFor="for">Password</FormLabel>
-                <FormInput type="password" required></FormInput>
-                <FormButton type="submit">Continue</FormButton>
-                <Text>Forgot password</Text>
-              </Form>
-            </FormContent>
-          </Icon>
+          <Icon to="/">dolla</Icon>
+          <FormContent>
+            <Form>
+              <FormH1>Sign in to your account</FormH1>
+              <FormLabel htmlFor="for">Email</FormLabel>
+              <FormInput type="email" required></FormInput>
+              <FormLabel htmlFor="for">Password</FormLabel>
+              <FormInput type="password" required></FormInput>
+              <FormButton type="submit">Continue</FormButton>
+              <Text>Forgot password</Text>
+            </Form>
+          </FormContent>
         </FormWrap>
       </Container>
     </>
